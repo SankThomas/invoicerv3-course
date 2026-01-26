14f0c080f6a52b03ab332785586acece7a2d70a9
SankThomas <thomassankara74@gmail.com>
2026-01-27 02:03:54 +0300

    add remote patterns to images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "lh3.googleusercontent.com",
+        protocol: "https",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
