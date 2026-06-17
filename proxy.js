52419fbed052f7b413c579a607726bc311be2caa
SankThomas <thomassankara74@gmail.com>
2026-06-17 15:22:17 +0300

    add /terms and /privacy to public paths

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
 export default withAuth(async function proxy(req) {}, {
-  publicPaths: ["/"],
+  publicPaths: ["/", "/terms", "/privacy"],
 });
 
 export const config = {
